7e3b2e6a98f001fd5578f5c88cbb0656df5cc1c6
Evgeniy <evgeniyp@claned.com>
2018-11-22 14:56:39 +0300

    Remove wiki

--- config/initializers/gollum.rb
+++
@@ -1,6 +0,0 @@
-require 'gollum/app'
-
-Precious::App.set(:gollum_path, Rails.root.join('wiki').to_s)
-Precious::App.set(:default_markup, :markdown)
-wiki_options = { allow_editing: true }
-Precious::App.set(:wiki_options, wiki_options)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   mount Minerva::Engine => 'ims/rs/v1p0'
-  mount Precious::App, at: 'wiki'
 end
